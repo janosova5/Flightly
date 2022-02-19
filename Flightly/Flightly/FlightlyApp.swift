a41d07337315f56c0e1cf3dd8a6b2a8a42b1ec4e
Lenka Janosova <janosova.lenka@gmail.com>
2022-02-19 12:19:42 +0100

    Init xcode project

---
+++ Flightly/Flightly/FlightlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightlyApp.swift
+//  Flightly
+//
+//  Created by Lenka Janosova on 19/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct FlightlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
